4a62bc1b220ef2dcfbdae5b2ec499611de59b6b3
HristakaZ <hristaka2013@abv.bg>
2019-06-03 17:23:27 +0300

    Console Application comparing two numbers
    A console app, written in C#, comparing two numbers

---
+++ CompareTwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CompareTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.671376F;
+            float b = 7.436179173F;
+            bool equal = Math.Abs(a - b) > 0.000001;
+            Console.WriteLine(equal); //returns true.
+        }
+    }
+}
